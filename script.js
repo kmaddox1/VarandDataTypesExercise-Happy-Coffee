fb198f4a0a2bf645f7bd38bcd435232210104fa4
Kimberly Maddox <160989452+kmaddox1@users.noreply.github.com>
2024-04-30 22:48:06 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// Moring Routine
+
+var isHappy = false;
+var happyDance = "🕺🏻 Let's get this day started! 🕺";
+
+console.log("Starting morning...");
+console.log("Taylor is happy?");
+console.log(isHappy);
+
+console.log("Drinking coffee...☕️");
+isHappy = true;
+
+console.log("Taylor is happy?");
+console.log(isHappy);
+console.log(happyDance);
